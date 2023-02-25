3f0565c95ca448243bb6d4e295d045a50531cab2
Стив <kenethra.74@gmail.com>
2023-02-25 11:42:51 -0600

    stype: format code

--- log.go
+++ log.go
@@ -7,51 +7,49 @@ import (
 )
 
 var (
-  ErrMissingValue = fmt.Errorf("missing value")
+	ErrMissingValue = fmt.Errorf("missing value")
 )
 
 var _ Logger = &logger{}
 
 // logger is a logger that implements Logger
 type logger struct {
-  w io.Writer
-  keyvals []interface{}
+	w       io.Writer
+	keyvals []interface{}
 }
 
 // New return new logger
 func New() Logger {
-  l := &logger{}
+	l := &logger{}
 
-  if l.w == nil {
-    l.w = os.Stderr
-  }
-  
-  l.SetOutput(l.w)
+	if l.w == nil {
+		l.w = os.Stderr
+	}
 
-  return l
+	l.SetOutput(l.w)
+
+	return l
 }
 
 func (l *logger) log(msg interface{}, keyvals ...interface{}) {
-  var kvs []interface{}
+	var kvs []interface{}
 
-  if msg != nil {
-    m := fmt.Sprint(msg)
-    kvs = append(kvs, msgKey, m)
-  }
+	if msg != nil {
+		m := fmt.Sprint(msg)
+		kvs = append(kvs, msgKey, m)
+	}
 
-  // append logger fields
-  kvs = append(kvs, l.keyvals...)
-  if len(l.keyvals)%2 != 0 {
-    kvs = append(kvs, ErrMissingValue)
-  }
+	// append logger fields
+	kvs = append(kvs, l.keyvals...)
+	if len(l.keyvals)%2 != 0 {
+		kvs = append(kvs, ErrMissingValue)
+	}
 }
 
 func (l *logger) SetOutput(w io.Writer) {
-  l.w = w
+	l.w = w
 }
-
 
 func (l *logger) Debug(msg interface{}, keyvals ...interface{}) {
-  l.log(msg, keyvals...)
+	l.log(msg, keyvals...)
 }
-
