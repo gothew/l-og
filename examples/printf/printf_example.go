6cf84b8ec5190d0e3ae60afad3e9d95a02ac0f78
Стив <kenethra.74@gmail.com>
2023-03-21 19:27:21 -0600

    chore(example): example printf

---
+++ examples/printf/printf_example.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	log "github.com/gothew/l-og"
+)
+
+// 26 / 16 => 10
+// 1 / 16  => 1
+
+// 36 / 16 => 4
+// 2 / 16 => 2
+func main() {
+  log.Infof("%d", 0x1A)
+  log.Infof("%d", 0x24)
+}
+
